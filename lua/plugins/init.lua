fe2eb12dc8b4287f4902aa5b4d2512a731ba0dff
Alex Chau <chau.alex88@hotmail.com>
2025-11-27 19:05:15 +0000

    Update telescope to latest

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -81,8 +81,7 @@ return {
 	-- Autocompletion
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.2",
-		-- or                              , branch = '0.1.x',
+		tag = "v0.1.9",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		keys = {
 			{
